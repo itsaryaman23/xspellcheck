8f644c56b24eee333158251fd46072e352def634
Test <itsaryaman23@gmail.com>
2024-12-11 00:15:08 +0530

    pushing code for testing A2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const customDictionary = {
+    teh: "the",
+    wrok: "work",
+    fot: "for",
+    exampl: "example",
+  };
+
+  const handleChange = (e) => {
+    let word = e.target.value;
+    let arr = word.split(' ');
+    // console.log(customDictionary['teh'])
+
+    for(const a of arr)
+    {
+        if(customDictionary.hasOwnProperty(a))
+        {
+          document.getElementById('word').innerText = customDictionary[a];
+          document.getElementById('sent').style.display = "block";
+          break;
+        }
+        else{
+          document.getElementById('sent').style.display = "none";
+        }
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Spell Check and Auto-Correction</h1>
+      <textarea placeholder='Enter text...' onChange = {handleChange}></textarea>
+      <p id = 'sent'>Did you mean: <span id="word"></span>?</p>
     </div>
   );
 }
