4c9e0253bbe02cdd0b9183ed20dd09bfe7fde8b2
Test <itsaryaman23@gmail.com>
2024-12-11 00:54:22 +0530

    pushing code for testing A2

--- src/App.js
+++ src/App.js
@@ -14,21 +14,24 @@ function App() {
   const handleChange = (e) => {
     let word = e.target.value;
     let arr = word.split(' ');
-    // console.log(customDictionary['teh'])
-
-    for(const a of arr)
+    if(arr.length)
     {
-        if(customDictionary.hasOwnProperty(a))
-        {
-          document.getElementById('word').innerText = customDictionary[a];
-          document.getElementById('sent').style.display = "block";
-          break;
-        }
-        else{
-          document.getElementById('sent').style.display = "none";
-        }
-    }
 
+      
+      for(const a of arr)
+        {
+          if(customDictionary.hasOwnProperty(a))
+            {
+              document.getElementById('word').innerText = customDictionary[a];
+              document.getElementById('sent').style.display = "block";
+              break;
+            }
+            else{
+              document.getElementById('sent').style.display = "none";
+            }
+          }
+          
+        }
   }
 
   return (
